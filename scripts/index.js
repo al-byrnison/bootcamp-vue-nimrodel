1a2b7f246276a44fcad90c86be25af759acce4d1
AlByrnison <alfonsots09@gmail.com>
2023-04-13 02:36:38 -0400

    add bootstrap

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,38 @@
 //MENU ACTIONS
-const btnBurger = document.querySelector('#toggle-button');
-const menu = document.querySelector('.menu');
-const iconMenu = document.querySelector('#icon-toggle');
+const btnBurger = document.getElementById('toggle-burger');
+const iconToggle = document.getElementById('icon-toggle');
+btnBurger.addEventListener('click', function () {
+  iconToggle.classList.toggle('fa-burger');
+  iconToggle.classList.toggle('fa-drumstick-bite');
+});
 
-const toggleMenu = () => {
-  menu.classList.toggle('active');
-  iconMenu.classList.toggle('fa-burger');
-  iconMenu.classList.toggle('fa-drumstick-bite');
-};
-
-btnBurger.addEventListener('click', toggleMenu);
-
-//FORM ACTIONS
+//FORM DATE-TIME ACTIONS
 const reservationDate = document.getElementById('reservation-date');
 const reservationHour = document.getElementById('reservation-time');
-const btnSend = document.getElementById('btn-send');
-const btnReservation = document.getElementById('btn-reservation');
-
 const date = new Date();
 const threeWeeksLater = new Date(date.getTime() + 21 * 24 * 60 * 60 * 1000);
-
 reservationDate.min = reservationDate.value = date.toISOString().split('T')[0];
 reservationDate.max = threeWeeksLater.toISOString().split('T')[0];
 reservationHour.value = date.toLocaleTimeString('es-CL', {
   timeStyle: 'short',
 });
 
-btnSend.addEventListener('click', (e) => e.preventDefault());
-btnReservation.addEventListener('click', (e) => e.preventDefault());
+//VALIDATE FORM
+(() => {
+  'use strict';
+  const forms = document.querySelectorAll('.needs-validation');
+
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      'submit',
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      },
+      false
+    );
+  });
+})();
